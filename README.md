0c1603348d942b6c872a2b25307a4741d6da16d6
autoscatto <4utoscatto@gmail.com>
2014-05-07 21:05:17 +0200

    init

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 tntvillage-cp-provider
 ======================
 
-TNTVillage CouchPotato provider
+TNTVillage ([An italian ethical sharing forum](http://www.tntvillage.scambioetico.org/index.php) ![logo](http://forum.tntvillage.scambioetico.org/style_images/mkportal-636/loghino.gif) ) CouchPotato torrent provider.
+
+####SETUP INSTRUCTIONS
+
+```
+cd $CouchPotatoServerdir/couchpotato/core/providers/torrent/
+git clone https://github.com/autoscatto/tntvillage-cp-provider.git tntvillage
+
+# Now you should see **TNT Village** as one of the prodivers for Torrents. (Settings -> Searcher)
+# set your username/password in tntvillage module preference (same view Settings -> Searcher)
+# set *ita, italian (or similar) as preferred keywords (Settings -> Categories, Preferred field)
+# to get higher score for italian movie.
+# *OPTIONAL:* set additional score for tntvillage results in module preference
+```
+
+####SHOUT OUT
+
+Thanks to RuudBurger (https://github.com/RuudBurger) for developing CouchPotato, 
+and to bateman (https://github.com/bateman), for its corsaronero-cp-provider, that I used as a skeleton and from where I copied the function for title translation.
+
+
+
+License
+=========
+
+```
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2013 Romain Lespinasse <romain.lespinasse@gmail.com>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+```
---
+++ main.py
@@ -0,0 +1,170 @@
+from bs4 import BeautifulSoup
+from couchpotato.core.helpers.encoding import simplifyString, tryUrlencode
+from couchpotato.core.helpers.variable import tryInt
+from couchpotato.core.logger import CPLog
+from couchpotato.core.providers.torrent.base import TorrentMagnetProvider
+import datetime
+import traceback
+import re
+import requests
+import time
+log = CPLog(__name__)
+
+
+log = CPLog(__name__)
+
+
+class TNTVillage(TorrentMagnetProvider):
+
+    sess = None
+    last_login_check = None
+    payload = None
+
+
+    urls = {
+        'test': 'http://forum.tntvillage.scambioetico.org/index.php',
+        # TODO: understand how the fuck the super does login
+        #'login': 'http://forum.tntvillage.scambioetico.org/index.php',  
+        'base_url': 'http://forum.tntvillage.scambioetico.org/index.php',
+    }
+
+    http_time_between_calls = 1  # seconds
+    cat_backup_id = None
+
+    ### TODO: what about movie year and quality? ###
+    def _searchOnTitle(self, title, movie, quality, results):
+        self.login()
+        log.debug("Searching for %s (imdb: %s) (%s) on %s" % (title,
+                                                              movie['library']['identifier'].replace('tt', ''),
+                                                              quality['label'],
+                                                              self.urls['base_url']))
+
+        # Get italian title
+        # First, check cache
+        cache_key = 'italiantitle.%s' % movie['library']['identifier']
+        italiantitle = self.getCache(cache_key)
+
+        if not italiantitle:
+            try:
+                dataimdb = self.getHTMLData(
+                    'http://www.imdb.com/title/%s/releaseinfo' % (movie['library']['identifier']))
+                html = BeautifulSoup(dataimdb)
+                try:
+                    italiantitle = html.find('table', id='akas').find('td', text="Italy").findNext().text
+                except:
+                    log.debug(
+                        'Failed to find italian title for %s, it has probably never been released in Italy, '
+                        'we\'ll try searching for the original title anyways',
+                        title)
+                    italiantitle = title
+            except:
+                log.error('Failed parsing iMDB for italian title, using the original one: %s', traceback.format_exc())
+                italiantitle = title
+
+            self.setCache(cache_key, italiantitle, timeout=25920000)
+
+        log.debug("Title after searching for the italian one: %s" % italiantitle)
+
+        # remove accents
+        simpletitle = simplifyString(italiantitle)
+        payload = {'act': 'allreleases', 'st': '0', 'cat': '4', 'filter': simpletitle}
+        data = self.sess.get('http://forum.tntvillage.scambioetico.org/index.php', params=payload)
+        soup = BeautifulSoup(data.content)
+        row = soup.findAll('tr', attrs={'class': 'row4'})
+
+        if row and len(row) == 0:
+            log.info("No torrents found for %s on tntvillage.scambioetico.org", italiantitle)
+            return
+
+        if row:
+                try:
+                    self.parseResults(results, row)
+                except:
+                    log.error('Failed parsing ilCorsaroNero: %s', traceback.format_exc())
+
+        else:
+                log.debug('No search results found.')
+
+    # computes days since the torrent release
+    def ageToDays(self, age_str):
+        # actually a datetime.timedelta object
+        tdelta = datetime.datetime.now() - datetime.datetime.strptime(age_str, " %b %d %Y")
+        # to int
+        return tdelta.days
+
+    # retrieves the magnet link from the detail page of the original torrent result
+    def getMagnetLink(self, url):
+        data = self.sess.get(url)
+        html = BeautifulSoup(data.content)
+        magnet = html.find('a', href=re.compile(r'magnet:*'))['href']
+        titolo = html.findAll('td', id='sottotitolo')[1].text.strip('&nbsp;')
+        datas = html.findAll('span', attrs={"class": "postdetails"})[0].text
+        datas = re.findall(r':(.*?),', datas)[0]
+        data = self.ageToDays(datas)
+        return titolo, data, magnet
+
+    # filters the <td> elements containing the results, if any
+    def parseResults(self, results, entries):
+        new = {}
+        for result in entries:
+            tds = result.findAll('td')
+            if len(tds) != 9:
+                log.info("Wrong search result format, skipping.")
+                continue
+            try:
+                new['detail_url'] = tds[0].a['href']
+                new['size'] = self.parseSize("%s GB" % tds[7].span.text)
+                new['id'] = tds[0].a['href'].split('showtopic=')[1]
+                new['name'], new['age'], new['url'] = self.getMagnetLink(new['detail_url'])
+                new['seeders'] = tryInt(tds[5].span.text)
+                new['leechers'] = tryInt(tds[4].span.text)
+                new['score'] = self.conf('extra_score') + 0
+
+            except:
+                log.info("Search entry processing FAILED!")
+                continue
+
+            results.append(new)
+            log.debug("New result %s", new)
+
+    def login(self):
+        if not self.sess:
+            self.sess = requests.Session()
+        # Check if we are still logged in every hour/2
+        now = time.time()
+        if not self.last_login_check or not self.last_login_check < (now - 1800):
+            try:
+                login_param = {'act': 'Login', 'CODE': '01'}
+                auth = {'UserName': self.conf('username'), 'PassWord': self.conf('password')}
+                resp = self.sess.post(self.urls.get('test'),
+                                      params=login_param,
+                                      data=auth)
+                if self.loginCheckSuccess(resp.content):
+                    self.last_login_check = now
+                    return True
+            except:
+                pass
+            self.last_login_check = None
+
+        if self.last_login_check:
+            return True
+
+        try:
+            output = self.sess.get(self.urls.get('test'), params={'act': 'allreleases'})
+
+            if self.loginSuccess(output.content):
+                self.last_login_check = now
+                return True
+            error = 'unknown'
+        except:
+            error = traceback.format_exc()
+
+        self.last_login_check = None
+        log.error('Failed to login %s: %s', (self.getName(), error))
+        return False
+
+    def loginSuccess(self, output):
+        return "Spiacente" in output
+
+    def loginCheckSuccess(self, output):
+        return "Attendi mentre viene caricata la pagina..." in output
